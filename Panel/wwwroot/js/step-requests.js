5f18c5a44d84e9c38d8a03eb592f771b00aadaef
Mahdi Roudaki <mahdiroudaki@hotmail.com>
2019-12-14 10:54:28 +0330

    Haghighi/Step1

--- Panel/wwwroot/js/step-requests.js
+++ Panel/wwwroot/js/step-requests.js
@@ -3,40 +3,19 @@
     $(".next").click(function (e) {
         var form = "#step" + $(".tab-pane.active").attr("id").replace("tab2-", "");
         if ($(form).valid()) {
-            if ($(form).attr("id") == "step1") {
 
-                e.preventDefault();
+            e.preventDefault();
+            console.log($(form).serializeArray());
 
-                $.ajax({
-                    url: "/Haghighi/Step1",
-                    method: "POST",
-                    data: getFormData($(form)),
+            $.ajax({
+                url: "/Haghighi/" + $(form).attr("id"),
+                method: "POST",
+                data: $(form).serializeArray(),
 
-                }).done(function (response) {
-                    alert(response);
-                });
+            }).done(function (response) {
+                console.log(response);
+            });
 
-            } else if ($(form).attr("id") == "step2") {
-
-            } else if ($(form).attr("id") == "step3") {
-
-            } else if ($(form).attr("id") == "step4") {
-
-            } 
         }
     })
-
-    function getFormData($form) {
-        var unindexed_array = $form.serializeArray();
-        var indexed_array = {};
-
-        $.map(unindexed_array, function (n, i) {
-            indexed_array[n['name']] = n['value'];
-        });
-
-        console.log(unindexed_array);
-
-        return indexed_array;
-    }
-
 });
