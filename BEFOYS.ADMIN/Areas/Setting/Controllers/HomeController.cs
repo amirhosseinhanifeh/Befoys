e49874811c6ea078ad14bbc4c1f7d7c980e66404
Roozbeh Shamekhi <roozbehshamekhi@hotmail.com>
2019-12-30 10:35:42 +0330

    ui

---
+++ BEFOYS.ADMIN/Areas/Setting/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEFOYS.ADMIN.Areas.Setting.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Setting")]
+        [Route("Setting/[controller]/[action]/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
